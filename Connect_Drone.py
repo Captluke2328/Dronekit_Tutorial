5ea75bbb9717c310d01456d9951bd4b6da81eff7
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-08-03 00:23:13 +0800

    Update all connection protocol

--- Connect_Drone.py
+++ Connect_Drone.py
@@ -1,5 +1,26 @@
 from dronekit import *
 
-connection_string = '127.0.0.1:14550'
+'''
+    ## This is how to port forward data from Mavproxy (Ground Stations) to Copter (IP address)
+    10.60 address is from ifconfig of the connected wifi, to enable this run:
+    
+    USING USB  : mavproxy.py --master=/dev/ttyACM0 --out=udp:10.60.216.198
+    USING UART : mavproxy.py --master=/dev/ttyTHS1,921600 --out=udp:10.60.216.198:14550
+
+'''
+#connection_string = '10.60.216.198:14550'
+
+'''Using SiTL Connection'''
+#connection_string = '127.0.0.1:14550'
+
+'''Using Uart Serial Rx->Tx and Tx-Rx connection'''
+connection_string = '/dev/ttyTHS1,921600'
+
+''' Using USB Connection '''
+#connection_string = '/dev/ttyACM0'
+
 vehicle = connect(connection_string, wait_ready = True)
 print("Virtual Copter is Ready")
+~                                         
+
+
