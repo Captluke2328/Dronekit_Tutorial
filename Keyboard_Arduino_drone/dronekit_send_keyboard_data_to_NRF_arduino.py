51d0d88795683858aba161ca18e64e6b49a38f7a
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-09-07 12:16:03 +0800

    remove swap file

--- Keyboard_Arduino_drone/dronekit_send_keyboard_data_to_NRF_arduino.py
+++ Keyboard_Arduino_drone/dronekit_send_keyboard_data_to_NRF_arduino.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     
     init = KeyboardArduino()
     
-    ser = init.initConnection("/dev/cu.usbmodem1201","9600")
+    ser = init.initConnection("/dev/cu.usbmodem1101","9600")
     
     while True:
         vals = init.getKeyboardInput()  
